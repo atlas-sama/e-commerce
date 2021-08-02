8f13b4131ad31a567c40d4df687bd51fc5c0cb50
atlas-sama <harshbhardwaj909@gmail.com>
2021-08-02 22:34:21 +0530

    Updates

---
+++ precache-manifest.3efa4a12cbf09feeae04619d8107ce1f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59aaf835b6854e08cb490b85dd7a21a1",
+    "url": "/e-commerce/index.html"
+  },
+  {
+    "revision": "97b9ec0fcc5698d42449",
+    "url": "/e-commerce/static/js/2.a860b043.chunk.js"
+  },
+  {
+    "revision": "70af1bffd51a7281193fa8f9b7319597",
+    "url": "/e-commerce/static/js/2.a860b043.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9940959fd5669a00affa",
+    "url": "/e-commerce/static/js/main.f7c6622d.chunk.js"
+  },
+  {
+    "revision": "6773e1930929ac494ff7",
+    "url": "/e-commerce/static/js/runtime-main.32edfd74.js"
+  },
+  {
+    "revision": "422fec1079cce450353e924288f8313b",
+    "url": "/e-commerce/static/media/commerce.422fec10.png"
+  }
+]);
